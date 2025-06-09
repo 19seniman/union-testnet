7e2e351871a3e991164a1b1aa636904c290ccca9
19seniman <161638164+19seniman@users.noreply.github.com>
2025-06-10 02:45:39 +0700

    Update and rename newunion.js to lim.js

--- lim.js
+++ lim.js
@@ -27,7 +27,7 @@ const logger = {
   banner: () => {
     console.log(`${colors.cyan}${colors.bold}`);
     console.log(`---------------------------------------------`);
-    console.log(`  Union Testnet Auto Bot - Airdrop Insiders  `);
+    console.log(` 🍉🍉 19Seniman From Insiders - FREE PALESTINE 🍉🍉`);
     console.log(`---------------------------------------------${colors.reset}`);
     console.log();
   }
